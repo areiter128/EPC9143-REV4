fff571fe60b0486172481abafeb6aa0e39db69a4
Andreas Reiter - M91406 <andreas.reiter@microchip.com>
2020-04-01 11:50:04 +0200

    Imported short and long time pressed detection functons for switch driver to support switch-over to temperature screen

--- sources/switch/switch.c
+++ sources/switch/switch.c
@@ -9,11 +9,12 @@
 #include "switch.h"
 #include "config/dpsk3_hwdescr.h"
 #include "pwr_control/pwr_control.h"
+#include "lcd/lcd.h"
 
 // PRIVATE VARIABLE DELARATIONS
 
-volatile uint16_t sw_cnt = 0; // local counter of SWITCH_USER being pressed
-#define SWITCH_DEBOUNCE_DELAY_DEFAULT    199 // Switch needs to be pressed >200ms to trip a SWITCH BUTTON event
+#define SWITCH_DEBOUNCE_DELAY_DEFAULT    199    // Switch needs to be pressed >20ms to trip a SWITCH BUTTON event
+#define SWITCH_LONG_PRESS_DELAY_DEFAULT  4999   // Switch needs to be pressed >500ms to trip a SWITCH BUTTON event
 
 volatile SWITCH_OBJECT_t switch_button;
 
@@ -21,11 +22,16 @@ volatile uint16_t appSwitch_Initialize(void)
 {
     volatile uint16_t fres = 1;
     
-    if(switch_button.debounce_delay == 0) // if debounce delay has not been configured...
-        switch_button.debounce_delay = SWITCH_DEBOUNCE_DELAY_DEFAULT; // Set default debounce delay
-    SW_USER_INIT;
-
-    switch_button.status.enabled = true; // Turn on Switch Button
+    fres &= drv_Switch_Initialize(&switch_button);   
+    
+    switch_button.debounce_delay = SWITCH_DEBOUNCE_DELAY_DEFAULT;
+    switch_button.long_press_delay = SWITCH_LONG_PRESS_DELAY_DEFAULT;
+    switch_button.status.bits.enabled = true;
+    
+    switch_button.event_btn_down = &appSwitch_EventButtonDown;
+    switch_button.event_long_press = &appSwitch_EventButtonLongPress;
+    switch_button.event_pressed = NULL; // Event not used
+    switch_button.event_release = &appSwitch_EventButtonUp;
     
     return(fres);
 }
@@ -34,35 +40,32 @@ volatile uint16_t appSwitch_Execute(void)
 {
     volatile uint16_t fres = 1;
 
-    if (!switch_button.status.enabled)
-    {
-        switch_button.status.pressed = false; // Mark Switch as RELEASED
-        return(1);  // Exit function
-    }
-    
-	// Enable/Disable AGM when the user pushes the SW_USER button
-	if(!SW_USER_PORTx){ 
-		
-		if ((sw_cnt++ > SWITCH_DEBOUNCE_DELAY_DEFAULT) && (!switch_button.status.pressed)){
-			// Add code for switch button event
-			switch_button.status.pressed = true;
-		}
-	}
-	else if (SW_USER_PORTx && switch_button.status.pressed) {
-        switch_button.status.pressed = false; 
-    }
-	else
-	{ sw_cnt = 0; } // Reset switch counter
+    fres &= drv_Switch_Execute(&switch_button);
 
     return(fres);
+}
+
+volatile uint16_t appSwitch_EventButtonDown(void) {
+    Nop();
+    return(1);
+}
+
+volatile uint16_t appSwitch_EventButtonLongPress(void) {
+    
+    lcd.screen ^= 1;
+    return(1);
+}
+
+volatile uint16_t appSwitch_EventButtonUp(void) {
+
+    return(1);
 }
 
 volatile uint16_t appSwitch_Dispose(void) 
 {
     volatile uint16_t fres = 1;
     
-    switch_button.debounce_delay = 0;
-    SW_USER_INIT;
+    fres &= drv_Switch_Dispose(&switch_button);
 
     return(fres);
 }
--- sources/switch/switch.h
+++ sources/switch/switch.h
@@ -36,35 +36,11 @@
 #include <stdbool.h> // include standard boolean data types
 #include <stddef.h> // include standard definition data types
 
+#include "switch/drivers/drv_switch.h"
 
 #ifdef	__cplusplus
 extern "C" {
 #endif /* __cplusplus */
-
-typedef struct{
-    volatile unsigned     :1;   // Bit 0:  (reserved)
-    volatile unsigned     :1;   // Bit 1:  (reserved)
-    volatile unsigned     :1;   // Bit 2:  (reserved)
-    volatile unsigned     :1;   // Bit 3:  (reserved)
-    volatile unsigned     :1;   // Bit 4:  (reserved)
-    volatile unsigned     :1;   // Bit 5:  (reserved)
-    volatile unsigned     :1;   // Bit 6:  (reserved)
-    volatile unsigned     :1;   // Bit 7:  (reserved)
-    volatile unsigned     :1;   // Bit 8:  (reserved)
-    volatile unsigned     :1;   // Bit 9:  (reserved)
-    volatile unsigned     :1;   // Bit 10: (reserved)
-    volatile unsigned     :1;   // Bit 11: (reserved)
-    volatile unsigned     :1;   // Bit 12: (reserved)
-    volatile unsigned     :1;   // Bit 13: (reserved)
-    volatile bool pressed :1;   // Bit 14: Indicates if the button is pressed or not
-    volatile bool enabled :1;   // Bit 15: Enables/disables the Switch button object
-}__attribute__((packed)) SWITCH_STATUS_t;
-    
-// PUBLIC DATA TYPE DECLARATION
-typedef struct {
-    volatile SWITCH_STATUS_t status;    // Status word of the switch object
-    volatile uint16_t debounce_delay;   // Number of call cycles until a switch event is triggered
-}SWITCH_OBJECT_t;
     
 // PUBLIC VARIABLE DECLARATION
 extern volatile SWITCH_OBJECT_t switch_button;
@@ -73,6 +49,12 @@ extern volatile SWITCH_OBJECT_t switch_button;
 extern volatile uint16_t appSwitch_Initialize(void);
 extern volatile uint16_t appSwitch_Execute(void);
 extern volatile uint16_t appSwitch_Dispose(void);
+
+// Switch Button Events
+extern volatile uint16_t appSwitch_EventButtonDown(void);
+extern volatile uint16_t appSwitch_EventButtonPressed(void);
+extern volatile uint16_t appSwitch_EventButtonLongPress(void);
+extern volatile uint16_t appSwitch_EventButtonUp(void);
 
 #ifdef	__cplusplus
 }
