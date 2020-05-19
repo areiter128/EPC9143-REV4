8b1b6b1362bc8d8eb92aeaf2c738a69a406502dd
Andreas Reiter - M91406 <andreas.reiter@microchip.com>
2020-05-19 11:49:10 +0200

    Added EPC9143 Rev4.0 hardware description files

---
+++ epc9143.X/sources/config/epc9143_r40_hwdescr.c
@@ -0,0 +1,17 @@
+/*
+ * File:   sepic_hwdescr.c
+ * Author: M91406
+ *
+ * Created on July 25, 2019, 11:13 PM
+ */
+
+
+#include <xc.h> // include processor files - each processor file is guarded.  
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "config/epc9143_r40_hwdescr.h"
+
+
+
+
